3ae2453c57ccd1ae6891458c89c8b02c1c4e6b99
nesb01t <nesb01t@163.com>
2024-05-12 00:01:12 -0700

    first commit

---
+++ WaveSoundBusDemo/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using NAudio.Wave;
+
+namespace WaveSoundBusDemo
+{
+    public partial class MainWindow : Window
+    {
+        private WaveOutEvent waveOut;
+        private AudioFileReader audioFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 设置按钮点击事件
+            PlayButton.Click += PlayButton_Click;
+            PauseButton.Click += PauseButton_Click;
+            StopButton.Click += StopButton_Click;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Paused)
+            {
+                waveOut.Play();
+            }
+            else
+            {
+                // 加载并播放 Wave 文件
+                string filePath = "file_example_WAV_1MG.wav"; // 替换为实际的 Wave 文件路径
+                audioFile = new AudioFileReader(filePath);
+                waveOut = new WaveOutEvent();
+                waveOut.Init(audioFile);
+                waveOut.Play();
+            }
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+            {
+                waveOut.Pause();
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && (waveOut.PlaybackState == PlaybackState.Playing ||
+                                    waveOut.PlaybackState == PlaybackState.Paused))
+            {
+                waveOut.Stop();
+                audioFile.Position = 0; // 将播放位置重置为文件开头
+            }
+        }
+    }
+}
